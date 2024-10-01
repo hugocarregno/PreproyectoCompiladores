e0400d82a8c6cad0452fd13eb28ee673c0758893
Hugo Ezequiel Carreño <40708524+hugocarregno@users.noreply.github.com>
2024-10-01 12:28:40 -0300

    implementacion de clase 3 direciones

---
+++ Codigo3D.java
@@ -0,0 +1,13 @@
+class Codigo3D{
+    Etiqueta codOP;
+    Simbolo op1;
+    Simbolo op2;
+    Simbolo res;
+
+    public Codigo3D(Etiqueta codOP, Simbolo op1, Simbolo op2, Simbolo res){
+        this.codOP = codOP;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.res = res;
+    }
+}
